2d777c23e379571850d149d58017025050afd532
irfan-official <irfanofficial.work@gmail.com>
2026-01-28 00:15:37 +0600

    Remove dist from version control

--- dist/interfaces_and_types/ReqType.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ReqTypeEnum = void 0;
-var ReqTypeEnum;
-(function (ReqTypeEnum) {
-    ReqTypeEnum[ReqTypeEnum["WebApp"] = 0] = "WebApp";
-    ReqTypeEnum[ReqTypeEnum["MobileApp"] = 1] = "MobileApp";
-})(ReqTypeEnum || (exports.ReqTypeEnum = ReqTypeEnum = {}));
