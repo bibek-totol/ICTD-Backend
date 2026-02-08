78749fdb228f5881e9df235d2976a0136aa2272e
Bibek <bbibekbhowmick2001@gmail.com>
2026-02-08 12:34:32 +0600

    *

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,8 @@ app.use(
       `http://localhost:${clientPort2}`,
       `http://localhost:${clientPort3}`,
       `https://ictd-lab-gsi-project.vercel.app`,
-      `https://ictdlgov.vercel.app`
+      `https://ictdlgov.vercel.app`,
+      `https://ictd-lab-gsi-project-frontend.vercel.app`
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
